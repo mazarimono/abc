1216ac6643408f278911d299ea19bda8414bdd5d
mazarimono <mazarimono@gmail.com>
2019-09-30 23:16:10 +0900

    add os import

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly
 import plotly.express as px
+import os
 
 gapminder = plotly.data.gapminder()
 btm = html.Div([
